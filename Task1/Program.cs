7922cc3da9e36477b5712a7b7f0c066b2c175ffb
alexey02rus <101921891+alexey02rus@users.noreply.github.com>
2022-08-22 21:40:57 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static bool? Check(string str)
+        {
+            Dictionary<char, char> sk = new Dictionary<char, char>
+            {
+                {'(',')'},
+                {'{','}'},
+                {'[',']'},
+            };
+            bool cont = false;
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in str)
+            {
+                if (sk.ContainsKey(c))
+                {
+                    stack.Push(sk[c]);
+                    cont = true;
+                }
+                if (sk.ContainsValue(c))
+                {
+                    if (stack.Count == 0 || stack.Pop()!=c)
+                    {
+                        return false;
+                    }
+                    cont = true;
+                }
+            }
+            if (stack.Count == 0 && cont)
+                return true;
+            else if (cont)
+                return false;
+            else
+                return null;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            if (Check(str) == true)
+            {
+                Console.WriteLine("Скобки расставлены верно!");
+            }
+            else if (Check(str) == false)
+            {
+                Console.WriteLine("Скобки расставлены неверно!");
+            }
+            else
+            {
+                Console.WriteLine("Скобки отсутствуют в строке!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
